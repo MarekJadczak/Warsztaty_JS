02a5ae5be85d20cdfb2150561724038909e19b55
MarekJadczak <mrkjadczak@gmail.com>
2018-03-17 16:18:32 +0100

    more buttons

--- js/app.js
+++ js/app.js
@@ -17,10 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
     
      buttons[0].addEventListener('click', function() {
       var textArea = this.previousElementSibling;
-      textArea.style.display = 'block';
-      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+
+        // ten kod wykona się, gdy spełni się jeden z powyższych warunków
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+
+        // ten kod zadziała, gdy warunek podany w `if` nie będzie spełniony, czyli w przypadku
+        // gdy `style.display` jest inny niż `none` oraz gdy nie jest pusty
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+
     });
-        
     
     
     // tutaj będziemy umieszczać kod ze wszystkich zadań
