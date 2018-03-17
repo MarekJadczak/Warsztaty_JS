be1ef331560a8344dd166e465311fabb7ac7942f
MarekJadczak <mrkjadczak@gmail.com>
2018-03-17 16:30:06 +0100

    more buttons complete

--- js/app.js
+++ js/app.js
@@ -14,24 +14,33 @@ document.addEventListener('DOMContentLoaded', function() {
     
     
     var buttons = document.querySelectorAll('.read-more');
-    
-     buttons[0].addEventListener('click', function() {
-      var textArea = this.previousElementSibling;
-      if (textArea.style.display === 'none' || textArea.style.display === '') {
 
-        // ten kod wykona się, gdy spełni się jeden z powyższych warunków
-        textArea.style.display = 'block';
-        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
-      } else {
+  function showHide() {
 
-        // ten kod zadziała, gdy warunek podany w `if` nie będzie spełniony, czyli w przypadku
-        // gdy `style.display` jest inny niż `none` oraz gdy nie jest pusty
-        textArea.style.display = 'none';
-        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
-      }
+    var textArea = this.previousElementSibling;
+   
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    }
+  }
 
-    });
-    
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+  }
+    // buttons[0].addEventListener('click', function() {
+      //var textArea = this.previousElementSibling;
+      //if (textArea.style.display === 'none' || textArea.style.display === '') {
+        //textArea.style.display = 'block';
+        //this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      //} else {
+        //textArea.style.display = 'none';
+        //this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+    //  }
+    //});
     
     // tutaj będziemy umieszczać kod ze wszystkich zadań
 });
